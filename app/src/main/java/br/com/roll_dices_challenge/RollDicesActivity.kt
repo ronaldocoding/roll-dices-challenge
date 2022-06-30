9a22054708a586f6b2cba304b1cfe03ccf091596
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-06-30 11:43:07 -0400

    Create dice rolling logic

--- app/src/main/java/br/com/roll_dices_challenge/RollDicesActivity.kt
+++ app/src/main/java/br/com/roll_dices_challenge/RollDicesActivity.kt
@@ -14,6 +14,39 @@ class RollDicesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val view = binding.root
         setContentView(view)
+        setUpRollDicesButton()
+    }
+
+    private fun setUpRollDicesButton() {
+        val rollDicesButton = binding.rollDicesButton
+        rollDicesButton.setOnClickListener {
+            rollDices()
+        }
+    }
+
+    private fun rollDices() {
+        val nextLeftDice = getRandomDice()
+        val nextRightDice = getRandomDice()
+        changeDices(nextLeftDice, nextRightDice)
+    }
+
+    private fun changeDices(leftDiceRollResult: Int, rightDiceRollResult: Int) {
+        val nextLeftDiceId = getDiceId(leftDiceRollResult)
+        val nextRightDiceId = getDiceId(rightDiceRollResult)
+
+        val leftDice = binding.leftDice
+        val rightDice = binding.rightDice
+
+        leftDice.setImageResource(nextLeftDiceId)
+        rightDice.setImageResource(nextRightDiceId)
+    }
+
+    private fun getDiceId(diceNumber: Int): Int {
+        return this.resources.getIdentifier(
+            "dice_$diceNumber",
+            "string",
+            this.packageName
+        )
     }
 
     private fun getRandomDice(): Int = (1..6).random()
