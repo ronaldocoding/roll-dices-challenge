6c5eaddd851ab3f61f7da81a8dd8149f4f8c9bf1
ronaldocosta <ronaldocosta.developer@gmail.com>
2022-06-30 10:40:03 -0400

    Create roll dices activity and layout

---
+++ app/src/main/java/br/com/roll_dices_challenge/RollDicesActivity.kt
@@ -0,0 +1,19 @@
+package br.com.roll_dices_challenge
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.roll_dices_challenge.databinding.ActivityRollDicesBinding
+
+class RollDicesActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityRollDicesBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = binding.root
+        setContentView(view)
+    }
+
+}
